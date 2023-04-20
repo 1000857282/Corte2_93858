5d8f28425e13e1ebcb047ecefe8ef8d2faf96382
Nicol Castillo <ndcastillor@gmail.com>
2023-04-20 15:45:47 -0500

    actualizacion profesor

--- Clases/Sesion_12/busqueda.py
+++
@@ -1,9 +0,0 @@
-def busqueda_dicc():
-    while n !="salir":
-        n = input("Ingrese un key de busqueda")
-        if n == "salir":
-            break
-        if n not in dicc:
-            print("Error. Fuera de Rango")
-        else:
-            print(dicc[n])
--- Tareas/Taller/CSV/prueba.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def file_exists(filename,organization_data = os.getcwd()):
-	"""
-	Check if the specified file exists at the specified directory
-	"""
-	files = os.listdir(organization_data)
-	return filename in files
